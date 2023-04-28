6369d58493ce76d520f217ff288df45672d477be
phlepp1111 <phlpp.neumann@gmail.com>
2023-04-28 13:32:54 +0200

    some styling

--- script.js
+++ script.js
@@ -11,28 +11,34 @@ function createDOM(products){
     for(let i=0; i<products.products.length; i++){
         console.log(products.products[i]);
         const card = document.createElement("div");
-        card.setAttribute("id", "card "+(i+1));
+        card.setAttribute("class", "card "+(i+1));
         const title = document.createElement("h2");
-        title.setAttribute("id", "title "+(i+1));
+        title.setAttribute("class", "title "+(i+1));
         title.innerHTML=products.products[i].title;
         const image = document.createElement("img");
-        image.setAttribute("id", "image "+(i+1));
+        image.setAttribute("class", "image "+(i+1));
         image.src= products.products[i].images[0];        
         const description = document.createElement("p");
-        description.setAttribute("id", "description "+(i+1));
+        description.setAttribute("class", "description "+(i+1));
         description.innerHTML=products.products[i].description;
         const price = document.createElement("p");
-        price.setAttribute("id", "price "+(i+1));
+        price.setAttribute("class", "price "+(i+1));
         price.innerHTML=products.products[i].price+" €";
+        const anzahl = document.createElement("p");
+        anzahl.innerHTML = "Anzahl: 0";
+
         const addButton = document.createElement("button");
         addButton.setAttribute("id", "addButton "+(i+1));
+        addButton.innerHTML = "+";
         const subtractButton = document.createElement("button");
         subtractButton.setAttribute("id", "subtractButton "+(i+1));
+        subtractButton.innerHTML = "-";
 
         card.appendChild(title);
         card.appendChild(image);
         card.appendChild(description);
         card.appendChild(price);
+        card.appendChild(anzahl);
         card.appendChild(addButton);
         card.appendChild(subtractButton);
 
