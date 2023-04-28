a49febd8c983febe11af5f7322d62f720ed2eecf
phlepp1111 <phlpp.neumann@gmail.com>
2023-04-28 14:53:54 +0200

    working item counters

--- script.js
+++ script.js
@@ -5,41 +5,50 @@ function getProducts() {
   return fetch('https://dummyjson.com/products')
     .then(res => res.json())
     .then((products) => {
-      createDOM(products);
-      return products.products;
+        productItems.push(...products.products);
+        productItems.forEach((product) => {
+            product.anzahl = 0;
+        });
+        createDOM(products);
+        return products.products;
     });
 };
-
-getProducts().then((products) => {
-  productItems.push(...products);
-  console.log(productItems);
-});
+getProducts();
+console.log(productItems);
 
 function createDOM(products){
     for(let i=0; i<products.products.length; i++){
-        console.log(products.products[i]);
         const card = document.createElement("div");
         card.setAttribute("class", "card "+(i+1));
+
         const title = document.createElement("h2");
         title.setAttribute("class", "title "+(i+1));
         title.innerHTML=products.products[i].title;
+
         const image = document.createElement("img");
         image.setAttribute("class", "image "+(i+1));
-        image.src= products.products[i].images[0];        
+        image.src= products.products[i].images[0];  
+
         const description = document.createElement("p");
         description.setAttribute("class", "description "+(i+1));
         description.innerHTML=products.products[i].description;
+
         const price = document.createElement("p");
         price.setAttribute("class", "price "+(i+1));
         price.innerHTML=products.products[i].price+" €";
+
         const anzahl = document.createElement("p");
-        anzahl.innerHTML = "Anzahl: 0";
+        anzahl.setAttribute("id", "anzahl"+(i+1));
+        anzahl.innerHTML = "Anzahl: "+productItems[i].anzahl;
 
         const addButton = document.createElement("button");
         addButton.setAttribute("id", "addButton "+(i+1));
+        addButton.setAttribute("onclick", "add("+(i+1)+")");
         addButton.innerHTML = "+";
+
         const subtractButton = document.createElement("button");
         subtractButton.setAttribute("id", "subtractButton "+(i+1));
+        subtractButton.setAttribute("onclick", "subtract("+(i+1)+")");
         subtractButton.innerHTML = "-";
 
         card.appendChild(title);
@@ -55,4 +64,19 @@ function createDOM(products){
     }
 };
 
-// getProducts();
+function add(x){
+    let anzahl = document.getElementById("anzahl"+x);
+    productItems[x].anzahl++;
+    anzahl.innerText=("Anzahl: ")+productItems[x].anzahl;
+    console.log(productItems[x].anzahl);
+}
+
+function subtract(x){
+    let anzahl = document.getElementById("anzahl"+x);
+    if(productItems[x].anzahl>0){
+        productItems[x].anzahl--;
+        anzahl.innerText=("Anzahl: ")+productItems[x].anzahl;
+        console.log(productItems[x].anzahl);
+    }
+    
+}
