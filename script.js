783816aebf683dd153bf038119ff59b9b1c96d9e
phlepp1111 <phlpp.neumann@gmail.com>
2023-04-29 18:53:11 +0200

    fixed spacing for better readability, fixed total amount not resetting after paying and some styling

--- script.js
+++ script.js
@@ -3,67 +3,67 @@ const warenkorbContainer = document.getElementById("warenkorbSub");
 const warenkorb = document.getElementById("warenkorb");
 const productItems = [];
 let warenkorbToggle = 0;
+
 function getProducts() {
-  return fetch('https://dummyjson.com/products')
-    .then(res => res.json())
-    .then((products) => {
-        productItems.push(...products.products);
-        productItems.forEach((product) => {
-            product.anzahl = 0;
+    return fetch("https://dummyjson.com/products")
+        .then((res) => res.json())
+        .then((products) => {
+            productItems.push(...products.products);
+            productItems.forEach((product) => {
+                product.anzahl = 0;
+            });
+            createDOM();
+            total();
         });
-        createDOM();
-        total();
-        return products.products;
-    });
-};
+}
 getProducts();
 
-function createDOM(){
-    container.innerHTML="";
-    productItems.map((_, i)=>{
+function createDOM() {
+    container.innerHTML = "";
+    productItems.map((_, i) => {
         let card = document.createElement("div");
-        card.setAttribute("class", "card "+(i+1));
+        card.setAttribute("class", "card " + (i + 1));
 
         let titleContainer = document.createElement("div");
-        titleContainer.setAttribute("class", "titleContainer "+(i+1));
+        titleContainer.setAttribute("class", "titleContainer " + (i + 1));
 
         let title = document.createElement("h3");
-        title.setAttribute("class", "title "+(i+1));
-        title.innerHTML=productItems[i].title;
+        title.setAttribute("class", "title " + (i + 1));
+        title.innerHTML = productItems[i].title;
 
         let image = document.createElement("img");
-        image.setAttribute("class", "image "+(i+1));
-        image.src= productItems[i].images[0];  
+        image.setAttribute("class", "image " + (i + 1));
+        image.src = productItems[i].images[0];
 
         let descContainer = document.createElement("div");
-        descContainer.setAttribute("class", "descContainer "+(i+1));
+        descContainer.setAttribute("class", "descContainer " + (i + 1));
 
         let description = document.createElement("p");
-        description.setAttribute("class", "description "+(i+1));
-        description.innerHTML=productItems[i].description;
+        description.setAttribute("class", "description " + (i + 1));
+        description.innerHTML = productItems[i].description;
 
         let priceContainer = document.createElement("div");
-        priceContainer.setAttribute("class", "priceContainer "+(i+1));
+        priceContainer.setAttribute("class", "priceContainer " + (i + 1));
 
         let price = document.createElement("p");
-        price.setAttribute("class", "price "+(i+1));
-        price.innerHTML=productItems[i].price+" €";
+        price.setAttribute("class", "price " + (i + 1));
+        price.innerHTML = productItems[i].price + " €";
 
         let anzahl = document.createElement("p");
-        anzahl.setAttribute("id", "anzahl"+(i+1));
-        anzahl.innerHTML = "Anzahl: "+productItems[i].anzahl;
+        anzahl.setAttribute("id", "anzahl" + (i + 1));
+        anzahl.innerHTML = "Anzahl: " + productItems[i].anzahl;
 
         let buttonContainer = document.createElement("div");
-        buttonContainer.setAttribute("class", "buttonContainer "+(i+1));
+        buttonContainer.setAttribute("class", "buttonContainer " + (i + 1));
 
         let addButton = document.createElement("button");
-        addButton.setAttribute("id", "addButton "+(i+1));
-        addButton.setAttribute("onclick", "add("+(i+1)+")");
+        addButton.setAttribute("id", "addButton " + (i + 1));
+        addButton.setAttribute("onclick", "add(" + (i + 1) + ")");
         addButton.innerHTML = "+";
 
         let subtractButton = document.createElement("button");
-        subtractButton.setAttribute("id", "subtractButton "+(i+1));
-        subtractButton.setAttribute("onclick", "subtract("+(i+1)+")");
+        subtractButton.setAttribute("id", "subtractButton " + (i + 1));
+        subtractButton.setAttribute("onclick", "subtract(" + (i + 1) + ")");
         subtractButton.innerHTML = "-";
 
         let br = document.createElement("br");
@@ -75,101 +75,99 @@ function createDOM(){
         card.appendChild(descContainer);
         priceContainer.appendChild(price);
         priceContainer.appendChild(anzahl);
-
         buttonContainer.appendChild(addButton);
         buttonContainer.appendChild(subtractButton);
         priceContainer.appendChild(buttonContainer);
-        
         card.appendChild(priceContainer);
         card.appendChild(br);
 
         container.appendChild(card);
     });
-};
-function warenkorbUpdate(){
-    warenkorbContainer.innerHTML = '';
+}
+
+function warenkorbUpdate() {
+    warenkorbContainer.innerHTML = "";
     productItems
-        .filter(item => item.anzahl > 0)
+        .filter((item) => item.anzahl > 0)
         .map((_, i) => {
-        
-                let item = document.createElement("div");
-                item.setAttribute("class", "item");
+            let item = document.createElement("div");
+            item.setAttribute("class", "item");
 
-                let title = document.createElement("p");
-                title.setAttribute("class", "title "+(i+1));
-                title.innerHTML=_.title;
+            let title = document.createElement("p");
+            title.setAttribute("class", "title " + (i + 1));
+            title.innerHTML = _.title;
 
-                let price = document.createElement("p");
-                price.setAttribute("class", "price "+(i+1));
-                price.innerHTML=_.price+" €";
+            let price = document.createElement("p");
+            price.setAttribute("class", "price " + (i + 1));
+            price.innerHTML = _.price + " €";
 
-                let addButton = document.createElement("button");
-                addButton.setAttribute("id", "addButton "+(i+1));
-                addButton.setAttribute("onclick", "add("+(_.id)+")");
-                addButton.innerHTML = "+";
+            let addButton = document.createElement("button");
+            addButton.setAttribute("id", "addButton " + (i + 1));
+            addButton.setAttribute("onclick", "add(" + _.id + ")");
+            addButton.innerHTML = "+";
 
-                let subtractButton = document.createElement("button");
-                subtractButton.setAttribute("id", "subtractButton "+(i+1));
-                subtractButton.setAttribute("onclick", "subtract("+(_.id)+")");
-                subtractButton.innerHTML = "-";
+            let subtractButton = document.createElement("button");
+            subtractButton.setAttribute("id", "subtractButton " + (i + 1));
+            subtractButton.setAttribute("onclick", "subtract(" + _.id + ")");
+            subtractButton.innerHTML = "-";
 
-                let anzahl = document.createElement("p");
-                anzahl.setAttribute("id", "anzahl"+(i+1));
-                anzahl.innerHTML = "Anzahl: "+_.anzahl;
- 
-                item.appendChild(title);
-                item.appendChild(price);
-                item.appendChild(addButton);
-                item.appendChild(subtractButton);
-                item.appendChild(anzahl);
-                warenkorbContainer.appendChild(item);
+            let anzahl = document.createElement("p");
+            anzahl.setAttribute("id", "anzahl" + (i + 1));
+            anzahl.innerHTML = "Anzahl: " + _.anzahl;
 
-    });
-};
-function add(x){
-    let anzahl = document.getElementById("anzahl"+x);
-    productItems[x-1].anzahl++;
-    anzahl.innerText=("Anzahl: ")+productItems[x-1].anzahl;
+            item.appendChild(title);
+            item.appendChild(price);
+            item.appendChild(addButton);
+            item.appendChild(subtractButton);
+            item.appendChild(anzahl);
+            warenkorbContainer.appendChild(item);
+        });
+}
+
+function add(x) {
+    let anzahl = document.getElementById("anzahl" + x);
+    productItems[x - 1].anzahl++;
+    anzahl.innerText = "Anzahl: " + productItems[x - 1].anzahl;
     total();
     warenkorbUpdate();
-};
+}
 
-function subtract(x){
-    let anzahl = document.getElementById("anzahl"+x);
-    if(productItems[x-1].anzahl>0){
-        productItems[x-1].anzahl--;
-        anzahl.innerText=("Anzahl: ")+productItems[x-1].anzahl;
+function subtract(x) {
+    let anzahl = document.getElementById("anzahl" + x);
+    if (productItems[x - 1].anzahl > 0) {
+        productItems[x - 1].anzahl--;
+        anzahl.innerText = "Anzahl: " + productItems[x - 1].anzahl;
         total();
         warenkorbUpdate();
-    } 
-};
+    }
+}
 
-function total(){
+function total() {
     let gesamtpreis = document.getElementById("total");
-    let total= 0;
-    for(let i=0; i<productItems.length; i++){
-        total+= productItems[i].anzahl*productItems[i].price;
-    };
-    gesamtpreis.innerText=("Gesamtpreis: "+total+" €");
+    let total = 0;
+    for (let i = 0; i < productItems.length; i++) {
+        total += productItems[i].anzahl * productItems[i].price;
+    }
+    gesamtpreis.innerText = "Gesamtpreis: " + total + " €";
+}
 
-};
-
-function toggleWarenkorb(){
-    if (warenkorbToggle == 0){
-        warenkorb.style.visibility="visible";
+function toggleWarenkorb() {
+    if (warenkorbToggle == 0) {
+        warenkorb.style.visibility = "visible";
         warenkorbToggle = 1;
-    } else{
-        warenkorb.style.visibility="hidden";
+    } else {
+        warenkorb.style.visibility = "hidden";
         warenkorbToggle = 0;
     }
 }
 
-function bezahlen(){
-    warenkorbContainer.innerHTML = '';
+function bezahlen() {
+    warenkorbContainer.innerHTML = "";
     toggleWarenkorb();
     productItems.forEach((product) => {
         product.anzahl = 0;
     });
+    total();
     createDOM();
-    alert('alles bezahlt.')
+    alert("alles bezahlt.");
 }
